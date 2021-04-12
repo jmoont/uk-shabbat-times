002ba5194d47b36efd00668a7bf4cbc244c60d06
Josh Moont <josh@weswap.com>
2021-04-12 08:42:13 +0100

    removed nodes

--- scrape.py
+++ scrape.py
@@ -84,7 +84,7 @@ def get_data_shabbat(url, pageno, itemsperpage, valid):
         var = row.get_text()
         var = var.split('\n')
         tab_row = {}
-        if var[3].strip() != "" and var[0].strip() != "* Mevarachim Hachodesh (Blessing the New Moon)" and not var[0].strip().startswith("Fast") and idx >= startno and idx < startno + itemsperpage and var[0].strip() != "" and var[2].strip() != "" and var[2].strip() != "&nbsp;" and var[0].strip() != "Parasha": 
+        if var[3].strip() != "" and var[0].strip() != "* Mevarachim Hachodesh (Blessing the New Moon)" and not var[0].strip().startswith("Fast") and not var[0].strip().startswith("Earliest")  and not var[0].strip().startswith("Erev") and idx >= startno and idx < startno + itemsperpage and var[0].strip() != "" and var[2].strip() != "" and var[2].strip() != "&nbsp;" and var[0].strip() != "Parasha": 
             print(var)
             sys.stdout.flush()
             hebrew_date = get_hebrew_date(var[1].strip())
@@ -102,6 +102,9 @@ def get_data_shabbat(url, pageno, itemsperpage, valid):
             tab_row["EnglishDate"] = get_english_date(var[1].strip(),var[3].strip().replace("Fev", "Feb"))
             tab_row["NextPage"] = "https://uk-shabbat-times.herokuapp.com/shabbat_times/page/" + str(pageno + 1)
             if post_dates[2] == True and valid == True:
+                del tab_row["NextPage"]
+                del tab_row["PostDate"]
+                del tab_row["ExpiryDate"]
                 tab.append(tab_row)
                 break
             elif valid == False:
